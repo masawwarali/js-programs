335166ef51ae484af431ebe433e1e5b979267b95
masawwarali <46315382+masawwarali@users.noreply.github.com>
2024-02-27 09:07:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,24 +4,24 @@
 }
 
 //Q2.
-// function sum(a, b) {
-//   return a + b;
-// }
-// console.log(sum(1, '2'));
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(1, '2'));
 
 //Q3.
-// let number = 0;
-// console.log(number++);
-// console.log(++number);
-// console.log(number);
+let number = 0;
+console.log(number++);
+console.log(++number);
+console.log(number);
 
 //Q4.
-// console.log('I want pizza'[0]);
+console.log('I want pizza'[0]);
 
 // Q5.
-// console.log([0] == false);
-// if ([0]) {
-//   console.log("I'm True");
-// } else {
-//   console.log("I'm False");
-// } 
+console.log([0] == false);
+if ([0]) {
+  console.log("I'm True");
+} else {
+  console.log("I'm False");
+} 
